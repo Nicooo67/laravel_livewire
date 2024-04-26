227e0da75067923b67fa0c9968074b6c36e728d7
KLEIN <nicolas.klein5@etu.unistra.fr>
2024-04-26 12:41:22 +0100

    update

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -8,13 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }} - Controle Laravel</title>
+    <title>{{ $title }} - Laravel</title>
     @vite(['resources/css/app.css'])
 </head>
 
 <body>
     <header class="bg-gray-500 text-white">
-        <h1 class="px-2 py-1">Controle Laravel</h1>
+        <h1 class="px-2 py-1">Laravel</h1>
         <nav class="bg-gray-700">
             <ul class="flex space-x-4 px-2 py-1">
                 <li>
